a073632683db94df6b779b5046b6c69517e369cc
Arsen Mamikonyan <arsen@mamikonyan.am>
2016-04-18 23:39:24 -0400

    Variational Inference for Dirichlet Process Gaussian Mixture

--- src/main/java/macrobase/analysis/transform/BatchScoreFeatureTransform.java
+++ src/main/java/macrobase/analysis/transform/BatchScoreFeatureTransform.java
@@ -4,7 +4,11 @@ import macrobase.analysis.stats.BatchTrainScore;
 import macrobase.conf.ConfigurationException;
 import macrobase.conf.MacroBaseConf;
 import macrobase.datamodel.Datum;
+import macrobase.diagnostics.JsonUtils;
+import macrobase.diagnostics.MetricsAndDensity;
 
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -28,8 +32,28 @@ public class BatchScoreFeatureTransform extends BatchTransform {
     protected List<Datum> transform(List<Datum> data) {
         batchTrainScore.train(data);
         List<Datum> results = new ArrayList<>(data.size());
+        List<MetricsAndDensity> metricsAndDensities = null;
+        boolean dumpScores = false;
+        String scoreFile = conf.getString(MacroBaseConf.SCORE_DUMP_FILE_CONFIG_PARAM, "");
+        if (scoreFile != "") {
+            metricsAndDensities = new ArrayList<>(data.size());
+            dumpScores = true;
+        }
         for(Datum d : data) {
+            double score = batchTrainScore.score(d);
             results.add(new Datum(d, batchTrainScore.score(d)));
+            if (dumpScores) {
+                metricsAndDensities.add(new MetricsAndDensity(d.getMetrics(), score));
+            }
+        }
+        if (dumpScores) {
+            try {
+                JsonUtils.dumpAsJson(metricsAndDensities, scoreFile);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
         }
         return results;
     }
